25b6bf29f118d1d41a4f035e049e5d38a45e3da3
Josh Humphries <jh@fullstory.com>
2019-05-02 12:05:44 -0400

    major re-org of goprotoc code: split into multiple files and start addressing some TODOs

--- cmd/goprotoc/codec.go
+++ cmd/goprotoc/codec.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"io"
+
+	"github.com/jhump/protoreflect/desc"
+)
+
+func doEncode(encodeType string, fds []*desc.FileDescriptor, r io.Reader) error {
+	// TODO
+	return nil
+}
+
+func doDecode(decodeType string, fds []*desc.FileDescriptor, r io.Reader) error {
+	// TODO
+	return nil
+}
+
+func doDecodeRaw(fds []*desc.FileDescriptor, r io.Reader) error {
+	// TODO
+	return nil
+}
--- cmd/goprotoc/free_fields.go
+++ cmd/goprotoc/free_fields.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"io"
+
+	"github.com/jhump/protoreflect/desc"
+)
+
+func doPrintFreeFieldNumbers(fds []*desc.FileDescriptor, w io.Writer) error {
+	// TODO
+	return nil
+}
