b302de9b29faf6f142c2cf7886e19bd8de696f28
Josh Humphries <jh@fullstory.com>
2019-05-07 18:06:33 -0400

    add usage message

--- cmd/goprotoc/goprotoc.go
+++ cmd/goprotoc/goprotoc.go
@@ -110,7 +110,90 @@ func fail(message string) {
 }
 
 func usage(exitCode int) {
-	// TODO
+	fmt.Printf(
+`Usage: %s [OPTION] PROTO_FILES
+Parse PROTO_FILES and generate output based on the options given:
+  -IPATH, --proto_path=PATH   Specify the directory in which to search for
+                              imports.  May be specified multiple times;
+                              directories will be searched in order.  If not
+                              given, the current working directory is used.
+  --version                   Show version info and exit.
+  -h, --help                  Show this text and exit.
+  --encode=MESSAGE_TYPE       Read a text-format message of the given type
+                              from standard input and write it in binary
+                              to standard output.  The message type must
+                              be defined in PROTO_FILES or their imports.
+  --decode=MESSAGE_TYPE       Read a binary message of the given type from
+                              standard input and write it in text format
+                              to standard output.  The message type must
+                              be defined in PROTO_FILES or their imports.
+  --decode_raw                Read an arbitrary protocol message from
+                              standard input and write the raw tag/value
+                              pairs in text format to standard output.  No
+                              PROTO_FILES should be given when using this
+                              flag.
+  --descriptor_set_in=FILES   Specifies a delimited list of FILES
+                              each containing a FileDescriptorSet (a
+                              protocol buffer defined in descriptor.proto).
+                              The FileDescriptor for each of the PROTO_FILES
+                              provided will be loaded from these
+                              FileDescriptorSets. If a FileDescriptor
+                              appears multiple times, the first occurrence
+                              will be used.
+  -oFILE,                     Writes a FileDescriptorSet (a protocol buffer,
+    --descriptor_set_out=FILE defined in descriptor.proto) containing all of
+                              the input files to FILE.
+  --include_imports           When using --descriptor_set_out, also include
+                              all dependencies of the input files in the
+                              set, so that the set is self-contained.
+  --include_source_info       When using --descriptor_set_out, do not strip
+                              SourceCodeInfo from the FileDescriptorProto.
+                              This results in vastly larger descriptors that
+                              include information about the original
+                              location of each decl in the source file as
+                              well as surrounding comments.
+  --print_free_field_numbers  Print the free field numbers of the messages
+                              defined in the given proto files. Groups share
+                              the same field number space with the parent
+                              message. Extension ranges are counted as
+                              occupied fields numbers.
+  --plugin=EXECUTABLE         Specifies a plugin executable to use.
+                              Normally, protoc searches the PATH for
+                              plugins, but you may specify additional
+                              executables not in the path using this flag.
+                              Additionally, EXECUTABLE may be of the form
+                              NAME=PATH, in which case the given plugin name
+                              is mapped to the given executable even if
+                              the executable's own name differs.
+  --<PLUGIN>_out=OUT_DIR      Invokes the plugin named <PLUGIN>, instructing
+                              it to generate source code into the given
+                              OUT_DIR. The given OUT_DIR can be in the
+                              extended form ARGS:OUT_DIR, in which case ARGS
+                              are extra arguments/flags to pass to the
+                              plugin.
+                              The plugin binary is located by searching for
+                              for any plugin locations configured with
+                              --plugin flags. If no such flags were provided
+                              for the named plugin, then an executable named
+                              'protoc-gen-<PLUGIN>' is used.
+                              If the named plugin is 'cpp', 'csharp', 'java',
+                              'javanano', 'js', 'objc', 'php', 'python', or
+                              'ruby' then the protoc binary is used to
+                              generate the output code (instead of some
+                              plugin).
+  @<filename>                 Read options and filenames from file. If a
+                              relative file path is specified, the file
+                              will be searched in the working directory.
+                              The --proto_path option will not affect how
+                              this argument file is searched. Content of
+                              the file will be expanded in the position of
+                              @<filename> as in the argument list. Note
+                              that shell expansion is not applied to the
+                              content of the file (i.e., you cannot use
+                              quotes, wildcards, escapes, commands, etc.).
+                              Each line corresponds to a single argument,
+                              even if it contains spaces.
+`, os.Args[0])
 	os.Exit(exitCode)
 }
 
