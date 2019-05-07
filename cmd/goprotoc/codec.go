cbec4ebc7342efc4dbbc4d3e0fd853ff933cb96f
Josh Humphries <jh@fullstory.com>
2019-05-07 17:47:40 -0400

    finish implementing various command-line options, including encoding/decoding and printing free field numbers

--- cmd/goprotoc/codec.go
+++ cmd/goprotoc/codec.go
@@ -1,22 +1,218 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
+	"math"
+	"strings"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/jhump/protoreflect/desc"
+	"github.com/jhump/protoreflect/dynamic"
 )
 
-func doEncode(encodeType string, fds []*desc.FileDescriptor, r io.Reader) error {
-	// TODO
+func doEncode(encodeType string, fds []*desc.FileDescriptor, r io.Reader, w io.Writer) error {
+	var md *desc.MessageDescriptor
+	for _, fd := range fds {
+		md = fd.FindMessage(encodeType)
+		if md != nil {
+			break
+		}
+	}
+	if md == nil {
+		return fmt.Errorf("type not defined: %s", encodeType)
+	}
+
+	var er dynamic.ExtensionRegistry
+	for _, fd := range fds {
+		er.AddExtensionsFromFileRecursively(fd)
+	}
+	dm := dynamic.NewMessageWithExtensionRegistry(md, &er)
+
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return fmt.Errorf("failed to read input: %v", err)
+	}
+	if err := dm.UnmarshalText(b); err != nil {
+		return fmt.Errorf("failed to parse input: %v", err)
+	}
+	b, err = dm.Marshal()
+	if err != nil {
+		return fmt.Errorf("failed to serialize message: %v", err)
+	}
+	_, err = w.Write(b)
+	if err != nil {
+		return fmt.Errorf("failed to write encoded message: %v", err)
+	}
 	return nil
 }
 
-func doDecode(decodeType string, fds []*desc.FileDescriptor, r io.Reader) error {
-	// TODO
+func doDecode(decodeType string, fds []*desc.FileDescriptor, r io.Reader, w io.Writer) error {
+	var md *desc.MessageDescriptor
+	for _, fd := range fds {
+		md = fd.FindMessage(decodeType)
+		if md != nil {
+			break
+		}
+	}
+	if md == nil {
+		return fmt.Errorf("type not defined: %s", decodeType)
+	}
+
+	var er dynamic.ExtensionRegistry
+	for _, fd := range fds {
+		er.AddExtensionsFromFileRecursively(fd)
+	}
+	dm := dynamic.NewMessageWithExtensionRegistry(md, &er)
+
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return fmt.Errorf("failed to read input: %v", err)
+	}
+	if err := dm.Unmarshal(b); err != nil {
+		return fmt.Errorf("failed to parse input: %v", err)
+	}
+	b, err = dm.MarshalTextIndent()
+	if err != nil {
+		return fmt.Errorf("failed to format message: %v", err)
+	}
+	_, err = w.Write(b)
+	if err != nil {
+		return fmt.Errorf("failed to write decoded message: %v", err)
+	}
 	return nil
 }
 
-func doDecodeRaw(fds []*desc.FileDescriptor, r io.Reader) error {
-	// TODO
-	return nil
+func doDecodeRaw(r io.Reader, w io.Writer) error {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	in := newCodedReader(data)
+	return decodeRawMessage(in, w, "", false)
+}
+
+func decodeRawMessage(in *codedReader, w io.Writer, indent string, inGroup bool) error {
+	for {
+		if in.eof() {
+			if inGroup {
+				return io.ErrUnexpectedEOF
+			}
+			return nil
+		}
+		t, wt, err := in.decodeTagAndWireType()
+		if err != nil {
+			return err
+		}
+		if wt == proto.WireEndGroup {
+			if inGroup {
+				return nil
+			}
+			return fmt.Errorf("input contains unexpected 'end group' wire type")
+		}
+		if t < 1 || t > maxTag {
+			return fmt.Errorf("input contains illegal tag number: %d", t)
+		}
+		if t >= specialReservedStart && t <= specialReservedEnd {
+			return fmt.Errorf("input contains illegal tag number: %d", t)
+		}
+		switch wt {
+		case proto.WireVarint:
+			v, err := in.decodeVarint()
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(w, "%s%d: %d\n", indent, t, v)
+		case proto.WireFixed32:
+			v, err := in.decodeFixed32()
+			if err != nil {
+				return err
+			}
+			f := math.Float32frombits(uint32(v))
+			fmt.Fprintf(w, "%s%d: %f\n", indent, t, f)
+		case proto.WireFixed64:
+			v, err := in.decodeFixed64()
+			if err != nil {
+				return err
+			}
+			f := math.Float64frombits(v)
+			fmt.Fprintf(w, "%s%d: %f\n", indent, t, f)
+		case proto.WireBytes:
+			v, err := in.decodeRawBytes(false)
+			if err != nil {
+				return err
+			}
+			if isProbablyMessage(v) {
+				fmt.Fprintf(w, "%s%d: <\n", indent, t)
+				nested := newCodedReader(v)
+				if err := decodeRawMessage(nested, w, indent + "  ", false); err != nil {
+					return err
+				}
+				fmt.Fprintf(w, "%s>\n", indent)
+			} else if isProbablyString(v) {
+				fmt.Fprintf(w, "%s%d: %s\n", indent, t, quoteString(v))
+			} else {
+				fmt.Fprintf(w, "%s%d: %s\n", indent, t, quoteBytes(v))
+			}
+		case proto.WireStartGroup:
+			fmt.Fprintf(w, "%s%d {\n", indent, t)
+			if err := decodeRawMessage(in, w, indent + "  ", true); err != nil {
+				return err
+			}
+			fmt.Fprintf(w, "%s}\n", indent)
+		default:
+			// invalid wire type
+			return fmt.Errorf("input contained invalid wire type: %d", wt)
+		}
+	}
+}
+
+func quoteString(s []byte) string {
+	// strings.Builder returns nil error for all Write* methods,
+	// so we ignore error return values in method calls below
+	var buf strings.Builder
+	// use WriteByte here to get any needed indent
+	_ = buf.WriteByte('"')
+	// Loop over the bytes, not the runes.
+	for i := 0; i < len(s); i++ {
+		// Divergence from C++: we don't escape apostrophes.
+		// There's no need to escape them, and the C++ parser
+		// copes with a naked apostrophe.
+		switch c := s[i]; c {
+		case '\n':
+			_, _ = buf.WriteString("\\n")
+		case '\r':
+			_, _ = buf.WriteString("\\r")
+		case '\t':
+			_, _ = buf.WriteString("\\t")
+		case '"':
+			_, _ = buf.WriteString("\\")
+		case '\\':
+			_, _ = buf.WriteString("\\\\")
+		default:
+			if c >= 0x20 && c < 0x7f {
+				_ = buf.WriteByte(c)
+			} else {
+				_, _ = fmt.Fprintf(&buf, "\\%03o", c)
+			}
+		}
+	}
+	_ = buf.WriteByte('"')
+	return buf.String()
+}
+
+func quoteBytes(b []byte) string {
+	// strings.Builder returns nil error for all Write* methods,
+	// so we ignore error return values in method calls below
+	var buf strings.Builder
+	// use WriteByte here to get any needed indent
+	_ = buf.WriteByte('"')
+	// Loop over the bytes, not the runes.
+	for i := 0; i < len(b); i++ {
+		// for bytes, we just hex-encode everything
+		_, _ = fmt.Fprintf(&buf, "\\%03o", b[i])
+	}
+	_ = buf.WriteByte('"')
+	return buf.String()
 }
--- cmd/goprotoc/goprotoc.go
+++ cmd/goprotoc/goprotoc.go
@@ -11,13 +11,19 @@
 // generation logic: it can only execute plugins to generate code. In order
 // to generate code that is built into the standard protoc (such as Python,
 // C++, Java, etc), this program can shell out to the standard protoc,
-// driving it as if it were a plugin.
+// driving it as if it were a plugin. In this mode, it provides to protoc
+// the file descriptors it has already parsed, instead of asking protoc to
+// re-parse all of the source code.
 package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/desc/protoparse"
 )
@@ -28,8 +34,7 @@ var gitSha = "" // can be replaced by -X linker flag
 
 func main() {
 	var opts protocOptions
-	err := parseFlags("", os.Args[1:], &opts, map[string]struct{}{})
-	if err != nil {
+	if err := parseFlags("", os.Args[1:], &opts, map[string]struct{}{}); err != nil {
 		fail(err.Error())
 	}
 
@@ -37,47 +42,62 @@ func main() {
 		fail("Only one of --descriptor_set_in and --proto_path can be specified.")
 	}
 
+	if len(opts.protoFiles) == 0 && !opts.decodeRaw {
+		fail("Missing input file.")
+	} else if len(opts.protoFiles) > 0 && opts.decodeRaw {
+		fail("When using --decode_raw, no input files should be given.")
+	}
+
 	var fds []*desc.FileDescriptor
-	if len(opts.inputDescriptors) > 0 {
-		var err error
-		if fds, err = loadDescriptors(opts.inputDescriptors); err != nil {
-			fail(err.Error())
-		}
-	} else {
-		p := protoparse.Parser{
-			ImportPaths:           opts.includePaths,
-			IncludeSourceCodeInfo: opts.includeSourceInfo,
-		}
-		var err error
-		if fds, err = p.ParseFiles(opts.protoFiles...); err != nil {
-			fail(err.Error())
+	if len(opts.protoFiles) > 0 {
+		if len(opts.inputDescriptors) > 0 {
+			var err error
+			if fds, err = loadDescriptors(opts.inputDescriptors, opts.protoFiles); err != nil {
+				fail(err.Error())
+			}
+		} else {
+			p := protoparse.Parser{
+				ImportPaths:           opts.includePaths,
+				IncludeSourceCodeInfo: opts.includeSourceInfo,
+			}
+			var err error
+			if fds, err = p.ParseFiles(opts.protoFiles...); err != nil {
+				fail(err.Error())
+			}
 		}
 	}
 
-	if len(opts.output) > 0 && opts.encodeType != "" {
+	doingCodeGen := len(opts.output) > 0 || opts.outputDescriptor != ""
+	if doingCodeGen && opts.encodeType != "" {
 		fail("Cannot use --encode and generate code or descriptors at the same time.")
 	}
-	if len(opts.output) > 0 && (opts.decodeType != "" || opts.decodeRaw) {
+	if doingCodeGen && (opts.decodeType != "" || opts.decodeRaw) {
 		fail("Cannot use --decode and generate code or descriptors at the same time.")
 	}
 	if opts.encodeType != "" && (opts.decodeType != "" || opts.decodeRaw) {
 		fail("Only one of --encode and --decode can be specified.")
 	}
 
+	var err error
 	switch {
 	case opts.encodeType != "":
-		err = doEncode(opts.encodeType, fds, os.Stdin)
+		err = doEncode(opts.encodeType, fds, os.Stdin, os.Stdout)
 	case opts.decodeType != "":
-		err = doDecode(opts.decodeType, fds, os.Stdin)
+		err = doDecode(opts.decodeType, fds, os.Stdin, os.Stdout)
 	case opts.decodeRaw:
-		err = doDecodeRaw(fds, os.Stdin)
+		err = doDecodeRaw(os.Stdin, os.Stdout)
 	case opts.printFreeFieldNumbers:
-		err = doPrintFreeFieldNumbers(fds, os.Stdout)
+		doPrintFreeFieldNumbers(fds, os.Stdout)
 	default:
-		if len(opts.output) == 0 {
+		if !doingCodeGen {
 			fail("Missing output directives.")
 		}
-		err = doCodeGen(opts.output, fds, opts.pluginDefs)
+		if len(opts.output) > 0 {
+			err = doCodeGen(opts.output, fds, opts.pluginDefs)
+		}
+		if err == nil && opts.outputDescriptor != "" {
+			err = saveDescriptor(opts.outputDescriptor, fds, opts.includeImports, opts.includeSourceInfo)
+		}
 	}
 	if err != nil {
 		fail(err.Error())
@@ -94,7 +114,103 @@ func usage(exitCode int) {
 	os.Exit(exitCode)
 }
 
-func loadDescriptors(fileNames []string) ([]*desc.FileDescriptor, error) {
-	// TODO
-	return nil,  nil
+func loadDescriptors(descFileNames []string, inputProtoFiles []string) ([]*desc.FileDescriptor, error) {
+	allFiles := map[string]*descriptor.FileDescriptorProto{}
+	for _, fileName := range descFileNames {
+		d, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			return nil, err
+		}
+		var set descriptor.FileDescriptorSet
+		if err := proto.Unmarshal(d, &set); err != nil {
+			return nil, fmt.Errorf("file %q is not a valid file descriptor set: %v", fileName, err)
+		}
+		for _, fd := range set.File {
+			if _, ok := allFiles[fd.GetName()]; !ok {
+				// only load into allFiles map if not already present: we keep
+				// only the first file found for a given name
+				allFiles[fd.GetName()] = fd
+			}
+		}
+	}
+	result := make([]*desc.FileDescriptor, len(inputProtoFiles))
+	linked := map[string]*desc.FileDescriptor{}
+	for i, protoName := range inputProtoFiles {
+		if _, ok := allFiles[protoName]; !ok {
+			return nil, fmt.Errorf("file not found: %q", protoName)
+		}
+		var err error
+		result[i], err = linkFile(protoName, allFiles, linked, nil)
+		if err != nil {
+			return nil, fmt.Errorf("could not load %q: %v", protoName, err)
+		}
+	}
+	return result, nil
+}
+
+func linkFile(fileName string, fds map[string]*descriptor.FileDescriptorProto, linkedFds map[string]*desc.FileDescriptor, seen []string) (*desc.FileDescriptor, error) {
+	for _, name := range seen {
+		if fileName == name {
+			seen = append(seen, fileName)
+			return nil, fmt.Errorf("cyclic imports: %v", strings.Join(seen, " -> "))
+		}
+	}
+	seen = append(seen, fileName)
+	fd, ok := linkedFds[fileName]
+	if ok {
+		return fd, nil
+	}
+	fdUnlinked, ok := fds[fileName]
+	if !ok {
+		return nil, fmt.Errorf("could not find dependency %q", fileName)
+	}
+	deps := make([]*desc.FileDescriptor, len(fdUnlinked.Dependency))
+	for i, dep := range fdUnlinked.Dependency {
+		var err error
+		deps[i], err = linkFile(dep, fds, linkedFds, seen)
+		if err != nil {
+			return nil, err
+		}
+	}
+	fd, err := desc.CreateFileDescriptor(fdUnlinked, deps...)
+	if err == nil {
+		linkedFds[fileName] = fd
+	}
+	return fd, err
+}
+
+func saveDescriptor(dest string, fds []*desc.FileDescriptor, includeImports, includeSourceInfo bool) error {
+	fdsByName := map[string]*descriptor.FileDescriptorProto{}
+	var fdSet descriptor.FileDescriptorSet
+	for _, fd := range fds {
+		toFileDescriptorSet(fdsByName, &fdSet, fd, includeImports, includeSourceInfo)
+	}
+	if b, err := proto.Marshal(&fdSet); err != nil {
+		return err
+	} else {
+		return ioutil.WriteFile(dest, b, 0666)
+	}
+}
+
+func toFileDescriptorSet(resultMap map[string]*descriptor.FileDescriptorProto, fdSet *descriptor.FileDescriptorSet, fd *desc.FileDescriptor, includeImports, includeSourceInfo bool) {
+	if _, ok := resultMap[fd.GetName()]; ok {
+		// already done this one
+		return
+	}
+
+	if includeImports {
+		for _, dep := range fd.GetDependencies() {
+			toFileDescriptorSet(resultMap, fdSet, dep, includeImports, includeSourceInfo)
+		}
+	}
+	fdp := fd.AsFileDescriptorProto()
+	if !includeSourceInfo {
+		// NB: this is destructive, so we need to do this step (part of saving
+		// to output descriptor set file) last, *after* descriptors (and any
+		// source code info) have already been used to do code gen
+		fdp.SourceCodeInfo = nil
+	}
+
+	resultMap[fd.GetName()] = fdp
+	fdSet.File = append(fdSet.File, fdp)
 }
